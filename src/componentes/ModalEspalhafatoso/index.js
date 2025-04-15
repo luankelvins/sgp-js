7609aacd7724ae947ebe157ac03a2389155c83a3
Luan Kelvin Da Silva <luankelvin@MacBook-Pro-de-Luan.local>
2025-04-15 15:36:33 -0300

    Atualização do EasterEgg

--- src/componentes/ModalEspalhafatoso/index.js
+++ src/componentes/ModalEspalhafatoso/index.js
@@ -3,7 +3,7 @@ import "../ModalEspalhafatoso/modalespalhafatoso.css";
 function ModalEspalhafatoso({ onClose }) {
   return (
     <div className="easter-overlay">
-      <div className="easter-modal-border">
+      <div className="easter-modal-border easter-animando">
         <div className="easter-modal-content">
           <h2 className="easter-mensagem">🧙‍♂️ Você é um mago curioso!</h2>
           <p className="easter-subtexto">
